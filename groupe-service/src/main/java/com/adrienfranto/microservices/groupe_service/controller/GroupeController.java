4123730942a130019ea53246729027c5006ee36d
adrien franto <radrienfrantos@gmail.com>
2025-08-31 22:20:32 +0300

    Ajouter des services travail et groupe

---
+++ groupe-service/src/main/java/com/adrienfranto/microservices/groupe_service/controller/GroupeController.java
@@ -0,0 +1,57 @@
+package com.adrienfranto.microservices.groupe_service.controller;
+
+import com.adrienfranto.microservices.groupe_service.dto.GroupeDto;
+import com.adrienfranto.microservices.groupe_service.model.Groupe;
+import com.adrienfranto.microservices.groupe_service.service.GroupeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/api/groupes")
+@RequiredArgsConstructor
+public class GroupeController {
+    private final GroupeService groupeService;
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public boolean inGroup(@RequestParam String nomGroupe,@RequestParam Integer quantite){
+        return groupeService.InGroup(nomGroupe,quantite);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public GroupeDto ajouterGroupe(@RequestBody GroupeDto dto) {
+        Groupe saved = groupeService.saveGroupe(dto);
+        return groupeService.mapToDto(saved);
+    }
+
+    @GetMapping("/list")
+    public List<GroupeDto> getAllGroupes() {
+        return groupeService.getAllGroupes()
+                .stream()
+                .map(groupeService::mapToDto)
+                .toList();
+    }
+
+    @GetMapping("/{id}")
+    public GroupeDto rechercheGroupe(@PathVariable Long id) {
+        Groupe groupe = groupeService.getGroupeById(id);
+        return groupeService.mapToDto(groupe);
+    }
+
+    @PutMapping("/{id}")
+    public GroupeDto modifierGroupe(@PathVariable Long id, @RequestBody GroupeDto dto) {
+        Groupe updated = groupeService.updateGroupe(id, dto);
+        return groupeService.mapToDto(updated);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteGroupe(@PathVariable Long id) {
+        groupeService.deleteGroupe(id);
+    }
+
+}
---
+++ travail-service/src/main/java/com/adrienfranto/microservices/travail_service/TravailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adrienfranto.microservices.travail_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravailServiceApplication.class, args);
+	}
+
+}
