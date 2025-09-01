0082adfffd25bce2e16597a1a257f801e3b4be83
adrien franto <radrienfranto2@gmail.com>
2025-09-01 14:10:37 +0300

    Ajouter corsFilter

---
+++ api-gateway/src/main/java/com/techie/microservices/api_gateway/config/CorsConfig.java
@@ -0,0 +1,29 @@
+package com.techie.microservices.api_gateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        // Mamela React dev server anao
+        config.setAllowedOrigins(List.of("http://192.168.56.1:5173"));
+        // Raha mila amin’ny rehetra, azonao atao: config.addAllowedOriginPattern("*");
+
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
